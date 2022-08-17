6ea73431ed36169ec250e5d0c5a6b574bfda2774
Nikita Astaforkin <astaforkin90@gmail.com>
2022-08-17 16:12:06 +0300

    add page.html

--- mynotebook/main/views.py
+++ mynotebook/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h4>Проверка работы</h4>')    
+    return render(request, 'main/index.html')    
 
 def page(request):
-    return HttpResponse('<h4>Страница блокнота</h4>')
+    return render(request, 'main/page.html')
