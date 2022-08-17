3b5f89a49787aedfeff2f13c577a9fab10055417
Nikita Astaforkin <astaforkin90@gmail.com>
2022-08-17 16:01:46 +0300

    add templates

--- mynotebook/main/views.py
+++ mynotebook/main/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h4>Проверка работы</h4>')    
+
+def page(request):
+    return HttpResponse('<h4>Страница блокнота</h4>')
